5ba0cdde969c8565eaf80506776a4f923044cdc8
Muhammad <muxammad_205@mail.ru>
2025-02-04 15:39:39 +0500

    feat(Dev): Dev

--- script.js
+++ script.js
@@ -1,105 +1,100 @@
 function updateActiveClass(items, currentIndex, activeClass) {
-  items?.forEach((item) => item.classList.remove(`${activeClass}`));
+    items.forEach((item) => item.classList.remove(activeClass));
 
-  if (items[currentIndex]) {
-    items[currentIndex].classList.add(`${activeClass}`);
-  }
+    if (items[currentIndex]) {
+        items[currentIndex].classList.add(activeClass);
+    }
 
-  return (currentIndex + 1) % items.length;
+    return (currentIndex + 1) % items.length;
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const sectionitems = document.querySelectorAll(".second-section-box");
-  const statItems = document.querySelectorAll(".stat-item");
-  let currentIndexSectionItems = 0;
-  let currentIndexStatItems = 0;
+    const sectionItems = document.querySelectorAll(".second-section-box");
+    const statItems = document.querySelectorAll(".stat-item");
+    let currentIndexSectionItems = 0;
+    let currentIndexStatItems = 0;
 
-  function updateSectionItems() {
-    currentIndexSectionItems = updateActiveClass(
-      sectionitems,
-      currentIndexSectionItems,
-      "active-second-section-box"
-    );
-  }
+    function updateSectionItems() {
+        currentIndexSectionItems = updateActiveClass(
+            sectionItems,
+            currentIndexSectionItems,
+            "active-second-section-box"
+        );
+    }
 
-  function updateStatItems() {
-    currentIndexStatItems = updateActiveClass(
-      statItems,
-      currentIndexStatItems,
-      "active-stat-item"
-    );
-  }
+    function updateStatItems() {
+        currentIndexStatItems = updateActiveClass(
+            statItems,
+            currentIndexStatItems,
+            "active-stat-item"
+        );
+    }
 
-  setInterval(updateSectionItems, 1800);
-  setInterval(updateStatItems, 1800);
+    setInterval(updateSectionItems, 1800);
+    setInterval(updateStatItems, 1800);
 
-  // SCROLL-BEHAVIOR
-  const body = document.querySelector(".container");
-  let isScrolling = false;
-  const sections = document.querySelectorAll(".section");
-  const dots = document.querySelectorAll(".nav-dot");
-  let scrollDirection;
-  const handleScroll = (e) => {
-    if (window.innerWidth > 1024) {
-      scrollDirection = e.wheelDeltaY;
-      const observer = new IntersectionObserver(
-        (entries) => {
-          entries.forEach((entry) => {
-            const sectionId = entry.target.id;
-            const currentDot = document.querySelector(
-              `.nav-dot[data-section=${sectionId}]`
+    // Scroll Behavior
+    const body = document.querySelector(".container");
+    let isScrolling = false;
+    const sections = document.querySelectorAll(".section");
+    const dots = document.querySelectorAll(".nav-dot");
+    let scrollDirection;
+
+    const handleScroll = (e) => {
+        if (window.innerWidth > 1024) {
+            scrollDirection = e.wheelDeltaY;
+            const observer = new IntersectionObserver(
+                (entries) => {
+                    entries.forEach((entry) => {
+                        const sectionId = entry.target.id;
+                        const currentDot = document.querySelector(`.nav-dot[data-section=${sectionId}]`);
+
+                        if (entry.isIntersecting) {
+                            entry.target.classList.add("fadeIn");
+                            entry.target.classList.remove("fadeOut");
+                            if (scrollDirection > 0) {
+                                dots.forEach((dot) => dot.classList.remove("second-active-dot"));
+                                dots.forEach((dot) => dot.classList.remove("active-dot"));
+
+                                setTimeout(() => {
+                                    currentDot.classList.add("second-active-dot");
+                                }, 400);
+                            } else {
+                                dots.forEach((dot) => dot.classList.remove("active-dot"));
+                                dots.forEach((dot) => dot.classList.remove("second-active-dot"));
+                                setTimeout(() => {
+                                    currentDot.classList.add("active-dot");
+                                }, 400);
+                            }
+                        } else {
+                            entry.target.classList.add("fadeOut");
+                            entry.target.classList.remove("fadeIn");
+                        }
+                    });
+                },
+                { threshold: 0.8 }
             );
 
-            if (entry.isIntersecting) {
-              entry.target.classList.add("fadeIn");
-              entry.target.classList.remove("fadeOut");
-              if (scrollDirection > 0) {
-                dots.forEach((dot) =>
-                  dot.classList.remove("second-active-dot")
-                );
-                dots.forEach((dot) => dot.classList.remove("active-dot"));
+            sections.forEach((section) => {
+                observer.observe(section);
+            });
+        }
 
-                setTimeout(() => {
-                  currentDot.classList.add("second-active-dot");
-                }, 400);
-              } else {
-                dots.forEach((dot) => dot.classList.remove("active-dot"));
-                dots.forEach((dot) =>
-                  dot.classList.remove("second-active-dot")
-                );
-                setTimeout(() => {
-                  currentDot.classList.add("active-dot");
-                }, 400);
-              }
+        e.preventDefault();
+        if (!isScrolling) {
+            isScrolling = true;
+
+            if (e.deltaY > 0) {
+                body.scrollTop += window.innerHeight;
             } else {
-              entry.target.classList.add("fadeOut");
-              entry.target.classList.remove("fadeIn");
+                body.scrollTop -= window.innerHeight;
             }
-          });
-        },
-        { threshold: 0.8 }
-      );
 
-      sections.forEach((section) => {
-        observer.observe(section);
-      });
-    }
+            setTimeout(() => {
+                isScrolling = false;
+            }, 500);
+        }
+    };
 
-    e.preventDefault();
-    if (!isScrolling) {
-      isScrolling = true;
-
-      if (e.deltaY > 0) {
-        body.scrollTop += window.innerHeight;
-      } else {
-        body.scrollTop -= window.innerHeight;
-      }
-
-      setTimeout(() => {
-        isScrolling = false;
-      }, 500);
-    }
-  };
-
-  window.addEventListener("wheel", handleScroll, { passive: false });
+    window.addEventListener("wheel", handleScroll, { passive: false });
 });
