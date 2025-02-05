f92fc504f6968da3fa68cd7f26654cf4d24e9621
Muhammad <muxammad_205@mail.ru>
2025-02-05 09:58:46 +0500

    fix(Safari): Safari support

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
-function updateActiveClass(items, currentIndex, activeClass) {
-  items?.forEach((item) => item.classList.remove(`${activeClass}`));
-
-  if (items[currentIndex]) {
-    items[currentIndex].classList.add(`${activeClass}`);
-  }
-
-  return (currentIndex + 1) % items.length;
-}
-
 document.addEventListener("DOMContentLoaded", function () {
-  const sectionitems = document.querySelectorAll(".second-section-box");
+ const sectionItems = document.querySelectorAll(".second-section-box");
   const statItems = document.querySelectorAll(".stat-item");
-  let currentIndexSectionItems = 0;
-  let currentIndexStatItems = 0;
 
-  function updateSectionItems() {
-    currentIndexSectionItems = updateActiveClass(
-      sectionitems,
-      currentIndexSectionItems,
-      "active-second-section-box"
-    );
+  if (!sectionItems.length || !statItems.length) return; // Prevent errors if elements are missing
+
+  let currentIndex = 0;
+
+  function updateActiveClass(items, currentIndex, activeClass) {
+    items.forEach((item) => item.classList.remove(activeClass));
+
+    if (items[currentIndex]) {
+      items[currentIndex].classList.add(activeClass);
+    }
+
+    return (currentIndex + 1) % items.length;
   }
 
-  function updateStatItems() {
-    currentIndexStatItems = updateActiveClass(
-      statItems,
-      currentIndexStatItems,
-      "active-stat-item"
-    );
+  function updateItems() {
+    requestAnimationFrame(() => {
+      currentIndex = updateActiveClass(sectionItems, currentIndex, "active-second-section-box");
+      updateActiveClass(statItems, currentIndex, "active-stat-item");
+    });
   }
 
-  setInterval(updateSectionItems, 1800);
-  setInterval(updateStatItems, 1800);
-
+  setInterval(updateItems, 1800);
   // SCROLL-BEHAVIOR
   const body = document.querySelector(".container");
   let isScrolling = false;
